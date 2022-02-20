1c2347d1060d63e3c01deb3242fd05e86df96127
Hassan Issa <hassan.issa2@outlook.com>
2022-02-20 15:36:01 -0500

    fixed win/lose overlay and uppercase letters

--- apps.js
+++ apps.js
@@ -2,24 +2,29 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const startButton = document.querySelector('.btn__reset');
 const ul = document.querySelector('ul');
+const overlay = document.getElementById('overlay');
 var missed = 0;
-
 
 
 // LISTEN FOR THE GAME TO BEGIN
 startButton.addEventListener('click', () => {
-    const overlay_start = document.getElementById('overlay');
-    overlay_start.style.display = 'none';
+    if (startButton.textContent != 'Start Game') {
+        location.reload();
+    } else {
+        const overlay_start = document.getElementById('overlay');
+        overlay_start.style.display = 'none';
+    }
 });
 
 
 
-// CREATE AN ARRAY
+// CREATE AN ARRAY OF 5 PHRASES
 const phrases = [ 
-    ['the sky is blue'],
-    ['my tea is hot'],
-    ['apple juice'],
-    ['the grass is green']
+    ['The sky is blue'],
+    ['My tea is hot'],
+    ['Apple juice'],
+    ['The grass is green'],
+    ['Go on a hike']
 ];
 
 
@@ -60,7 +65,7 @@ function checkLetter(button) {
     const li = document.getElementsByTagName('li')
     var match = null;
     for (let i = 0; i < li.length; i++) {
-        if (button.textContent === li[i].textContent) {
+        if (button.textContent === li[i].textContent.toLowerCase()) {
             li[i].className += 'show';
             match = button.textContent;
         } 
@@ -73,31 +78,42 @@ function checkLetter(button) {
 //LISTEN TO THE KEYBOARD
 qwerty.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON' && e.target.className != 'chosen') {
-        const button = e.target;
+        var button = e.target;
         button.className = 'chosen ';
         const letterFound = checkLetter(button);
+        button.disabled = 'true';
         if (!letterFound || e.target.className === 'chosen') {
+            const ol = document.querySelector('ol');
+            const olChild = ol.firstChild
             const liveHeart = document.getElementsByClassName('tries')[0];
+            var imgLost = document.createElement('img');
+            imgLost.src = 'images/lostHeart.png';
+            imgLost.style.width = '30px';
+            imgLost.style.height = '35px';
+            ol.insertBefore(imgLost, liveHeart);
             liveHeart.remove();
             missed += 1;
         }
+        checkWin()
     }   
-    checkWin()
 });
 
 
 
 //CHECK IF WIN OR LOSE GAME
 function checkWin() {
-
     const isLetter = document.getElementsByClassName('letter');
     const isShow = document.getElementsByClassName('show');
-    const overlay = document.getElementById('overlay');
+    const h2 = overlay.firstElementChild
     if (isLetter.length === isShow.length) {
         overlay.className += ' win '
         overlay.style.display = 'flex';
+        h2.textContent = 'You Win!'
+        startButton.textContent = 'Play again';
     } else if (missed > 4) {
         overlay.className += ' lose';
         overlay.style.display = 'flex';
+        h2.textContent = 'You Lose!'
+        startButton.textContent = 'Try again';
     }
 };
